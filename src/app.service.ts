4af25e22bcb0d93a5efdf725d70affdc142dc020
Tom Ray <tomwray13@gmail.com>
2023-11-23 13:09:12 +0000

    add ci-cd pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { LoggerService } from './core/logger/logger.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly logger: LoggerService) {}
-
   getHello(): string {
-    this.logger.log('Hello World!', 'AppService');
     return 'Hello World!';
   }
 }
