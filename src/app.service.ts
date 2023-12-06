a2a9941731cd59c014e6e8669d07df4d49fee310
tomwray13 <tomwray13@gmail.com>
2023-12-06 09:54:15 +0000

    add config module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
+    const environmentVariable = this.configService.get<string>(`environment`);
+    console.log(environmentVariable);
     return 'Hello World!';
   }
 }
