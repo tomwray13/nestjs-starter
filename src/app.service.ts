143d3a493569dcb9d7d3ab82306006be09ad9101
tomwray13 <tomwray13@gmail.com>
2023-12-13 09:24:03 +0000

    add cache

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { LoggerService } from './core/logger/logger.service';
 import { DatabaseService } from './database/database.service';
+import { CacheService } from './core/cache/cache.service';
 
 @Injectable()
 export class AppService {
@@ -8,13 +9,17 @@ export class AppService {
   constructor(
     private readonly logger: LoggerService,
     private readonly databaseService: DatabaseService,
+    private readonly cache: CacheService,
   ) {}
-  getHello() {
+  async getHello() {
     this.logger.log(`calling log from inside getHello method`, this.context, {
       userId: 123,
       isPremium: true,
     });
     this.databaseService.user.findMany();
+    this.cache.set(`key`, `VALUE FROM CACHE`, 1000);
+    const valueFromCache = await this.cache.get(`key`);
+    console.log(`valueFromCache`, valueFromCache);
     return `Hello world`;
   }
 }
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,11 +1,14 @@
 import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from '../config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TransformResponseInterceptor } from './interceptors/transform-response/transform-response.interceptor';
 import { LoggerService } from './logger/logger.service';
 import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 import { DatabaseService } from '../database/database.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheService } from './cache/cache.service';
 
 @Global()
 @Module({
@@ -13,6 +16,23 @@ import { DatabaseService } from '../database/database.service';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        const username = config.get(`redis.username`);
+        const password = config.get(`redis.password`);
+        return {
+          store: redisStore,
+          host: config.get(`redis.host`),
+          port: config.get(`redis.port`),
+          ...(username && { username }),
+          ...(password && { password }),
+          ttl: 10,
+          no_ready_check: true,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   providers: [
@@ -22,8 +42,9 @@ import { DatabaseService } from '../database/database.service';
     },
     LoggerService,
     DatabaseService,
+    CacheService,
   ],
-  exports: [LoggerService, DatabaseService],
+  exports: [LoggerService, DatabaseService, CacheService],
 })
 export class CoreModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
